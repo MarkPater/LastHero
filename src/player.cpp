be08825aaf938227bb4c8e75415da63dc6d8ad31
Mark Pater <mark.pater67@gmail.com>
2020-03-30 21:58:34 +0300

    Add movement to the right side

--- src/player.cpp
+++ src/player.cpp
@@ -9,7 +9,7 @@ Player::Player(float x, float y, sf::Texture & texture_sheet)
     createAnimationComponent(texture_sheet);
 
     m_animationComponent->addAnimation("IDLE_LEFT", 100.f, 0, 0, 13, 0, 192, 192);
-    m_animationComponent->addAnimation("WALK_LEFT", 100.f, 0, 1, 11, 1, 192, 192);
+    m_animationComponent->addAnimation("WALK", 100.f, 0, 1, 11, 1, 192, 192);
 }
 
 Player::~Player()
@@ -24,6 +24,13 @@ void Player::update(const float & dt)
         m_animationComponent->play("IDLE_LEFT", dt);
     }
     else if (m_movementComponent->getState(MovementState::MOVING_LEFT)) {
-        m_animationComponent->play("WALK_LEFT", dt);
+        m_sprite.setOrigin(0, 0);
+        m_sprite.setScale(1, 1);
+        m_animationComponent->play("WALK", dt);
+    }
+    else if (m_movementComponent->getState(MovementState::MOVING_RIGHT)) {
+        m_sprite.setOrigin(192, 0);
+        m_sprite.setScale(-1, 1);
+        m_animationComponent->play("WALK", dt);
     }
 }
